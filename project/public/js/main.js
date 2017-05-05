81267da0fc234ef44a318ac3009e41be0bfede33
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-05-05 16:08:40 -0600

    remove project

--- project/public/js/main.js
+++ project/public/js/main.js
@@ -1,9 +1,1 @@
 /* jshint esversion: 6 */
-
-angular
-    .module('mainApp', [])
-    .controller('mainCntrl', function() {
-    })
-    .component('uploadComponent', {
-        templateUrl: 'public/templates/uploadTemplate.html'
-    });
