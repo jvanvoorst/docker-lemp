2b3c492ce494399a5bd63f232b4e0007d798527a
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-05-05 17:24:53 -0600

    adding ajax

--- project/public/js/main.js
+++ project/public/js/main.js
@@ -2,8 +2,24 @@
 
 angular
     .module('mainApp', [])
-    .controller('mainCntrl', function() {
-    })
+    // .controller('mainCntrl', function() {
+    // })
     .component('uploadComponent', {
         templateUrl: 'public/templates/uploadTemplate.html'
+    })
+    .component('files', {
+        templateUrl: 'public/templates/files.html',
+        controller: function($http) {
+            $http.get('getFiles.php')
+                .then (function(res) {
+
+            })
+        }
     });
+    // .service('getFiles', function() {
+    //     this.files = [
+    //         'one.txt',
+    //         'two.txt',
+    //         'three.txt'
+    //     ];
+    // });
