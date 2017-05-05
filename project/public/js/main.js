633b86d0d22b8230d361f3f8844e0597538cb473
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-05-05 16:04:36 -0600

    add project

---
+++ project/public/js/main.js
@@ -0,0 +1,9 @@
+/* jshint esversion: 6 */
+
+angular
+    .module('mainApp', [])
+    .controller('mainCntrl', function() {
+    })
+    .component('uploadComponent', {
+        templateUrl: 'public/templates/uploadTemplate.html'
+    });
