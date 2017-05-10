60bc4f5b202c2651fe33a320861813ae11771b55
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-05-10 17:49:41 -0600

    fix path problem in upload

--- project/php/upload.php
+++ project/php/upload.php
@@ -1,9 +1,9 @@
 <?php
 
-echo $_FILES;
+var_dump($_FILES);
 
 $directory = 'uploads/';
-$file = $directory . basename($_FILES['fileToUpload']['name']);
+$file = $_SERVER['DOCUMENT_ROOT'] . '/uploads/' . basename($_FILES['fileToUpload']['name']);
 $extension = pathinfo($file, PATHINFO_EXTENSION);
 
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $file)) {
